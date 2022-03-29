b8d8bca2b99b97acd7ea044e6f799f81852c0bd9
Matouš Kozák <kozakmat@fit.cvut.cz>
2022-03-29 09:54:44 +0100

    footer init

--- LaCuartaAngular/src/app/app.module.ts
+++ LaCuartaAngular/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageHeaderComponent } from './page-header/page-header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PageFooterComponent } from './page-footer/page-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageHeaderComponent
+    PageHeaderComponent,
+    PageFooterComponent
   ],
   imports: [
     BrowserModule,
