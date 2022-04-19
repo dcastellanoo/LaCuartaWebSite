6650f208bf6c5131707adf380066920b4d55f82d
juanmamed <91466489+juanmamed@users.noreply.github.com>
2022-04-19 10:52:00 +0100

    bebidas y carta de vinos añadidos

--- LaCuartaAngular/src/app/app.module.ts
+++ LaCuartaAngular/src/app/app.module.ts
@@ -3,6 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 import { HttpClientModule } from "@angular/common/http";
 
+//Firebase configuration
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC1SQ9h7BfE2HwY4swj_LMGpJutNmHWkjE",
+  authDomain: "lacuartaweb-bce85.firebaseapp.com",
+  projectId: "lacuartaweb-bce85",
+  storageBucket: "lacuartaweb-bce85.appspot.com",
+  messagingSenderId: "434801223606",
+  appId: "1:434801223606:web:9512fef88e6e8ce99b4317",
+  measurementId: "G-2G2FRFGV8F"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
