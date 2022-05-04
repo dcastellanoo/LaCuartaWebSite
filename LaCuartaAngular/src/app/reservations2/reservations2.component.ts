7380d69f3e34c1e54f5cca664c1ee9b86b878cd6
dcastellanoo <diegobmg36@gmail.com>
2022-05-04 14:50:54 +0100

    Continuación reservas2

--- LaCuartaAngular/src/app/reservations2/reservations2.component.ts
+++ LaCuartaAngular/src/app/reservations2/reservations2.component.ts
@@ -3,6 +3,7 @@ import {Router} from "@angular/router";
 import {ReservationService} from "../services/reservation.service";
 import {EDayPeriod, Reservation} from "../model/reservation.model";
 import {FormBuilder} from "@angular/forms";
+import {AngularFirestore} from "@angular/fire/compat/firestore";
 
 
 @Component({
@@ -14,16 +15,19 @@ export class Reservations2Component implements OnInit {
   reservation: Reservation;
   people: number;
   horario: string;
+  reservation_times: string[];
 
   constructor(
     private fb: FormBuilder,
     public router: Router,
     public rs: ReservationService,
+    public db: AngularFirestore,
   ) {
     this.reservation = this.rs.getReservation();
     this.people = this.reservation.numAdults + this.reservation.numChilds;
     if (this.reservation.period == EDayPeriod.Lunch){
       this.horario = "Almuerzo";
+      this.reservation_times = this.db.collectionGroup('restaurant').
     } else {
       this.horario = "Cena";
     }
